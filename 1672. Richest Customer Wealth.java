6a544738702a31386ba76d1044ceea09e5835eb8
Vinayak Talukder <75025321+vinayaktalukder17@users.noreply.github.com>
2023-06-16 15:15:50 +0530

    Add files via upload

---
+++ 1672. Richest Customer Wealth.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int m=0;
+        for(int i=0;i<accounts.length;i++)
+        {
+            int sum=0;
+            for(int j=0;j<accounts[0].length;j++)
+            {
+                sum = sum+ accounts[i][j];
+            }
+            m =Math.max(m,sum);
+        }
+        return m;
+    }
+}
